a592443fe33deff3ac174a742690316d6aacf0fc
LilXoai <bido2107@gmail.com>
2024-11-19 18:19:43 -0800

    added arduino file

---
+++ workingcode/emlearn.h
@@ -0,0 +1,10 @@
+#ifndef MY_EMLEARN_H
+#define MY_EMLEARN_H
+
+#include <eml_trees.h>
+#include <eml_common.h>
+#include <eml_log.h>
+
+// Add any other necessary headers here
+
+#endif // MY_EMLEARN_H
