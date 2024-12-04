921ae70b708daf16333ad214466a7f96de499d13
caslam <117959404+caslam@users.noreply.github.com>
2024-12-04 10:26:44 -0800

    better ML output msg control for turning

--- teensy_final/teensy_final.ino
+++ teensy_final/teensy_final.ino
@@ -7,8 +7,6 @@
 #define IMU_ADDRESS 0x68 // IMU (AD0 to GND)
 #define LIDAR_ADDRESS 0x74
 
-enum msg_id {GYRO_MSG, LIDAR_MSG};
-
 // Instantiate the IMU
 MPU6050 IMU;
 calData calib = { 0 };
@@ -20,18 +18,25 @@ float offsetX1 = 0, offsetY1 = 0, offsetZ1 = 0;
 
 // Sliding window of gyro inputs to feed into model
 // gyroInputs is a circular buffer, while gyroToModel is a time-sorted copy of gyroInputs
+// gyroBuffer are three buffers to synchronize phone readings with IMU readings
 int16_t gyroInputs[36], gyroToModel[36];
-
+float gyroBuffer1[3], gyroBuffer2[3], gyroBuffer3[3];
 // Index for circular buffer
 int i = 0;
 
 int previousDistance1 = 0; // Previous distance for Wire (LIDAR 1)
 int previousDistance2 = 0; // Previous distance for Wire1 (LIDAR 2)
 int previousDistance3 = 0; // Previous distance for Wire2 (LIDAR 3)
-const int threshold = 1000; // Change threshold in mm
+const int threshold = 500; // Change threshold in mm
 const int buzzerPin = 20;   // Pin for the buzzer
 const int DETECTION_RANGE = 2000;
 
+// Threshold for setting turning flag based on phone gyro outputs
+const int rotation_threshold = 5;
+// Max value of turning flag, set when rotation_threshold is exceeded to
+// deal with the delay for model output
+const int turning_flag_max = 15;
+int turning = 0; // Flag for phone gyro measurements over certain value
 
 void setup() {
   Serial.begin(115200);
@@ -58,45 +63,85 @@ void setup() {
   Wire1.begin();
   Wire2.begin();
 
-  delay(50);
+  delay(20);
 }
 
-void loop() {
 
+void loop() {
   // Prints bluetooth data to the serial monitor
   printBluetoothData();
   
   // Prints gyro data to the serial monitor
   printGyroData();
 
-  // Computes the difference in gyro values
-  // Configure this one to accept bluetooth values. Do this after getting bluetooth to work.
-  int16_t gyroInput_x = (int16_t) ((gyroData.gyroX - offsetX1)- (phoneGyroData.gyroX))/2.0;
-  gyroInputs[3*i] = gyroInput_x;
+  // Updates the model input with new data
+  newModelInput();
 
-  int16_t gyroInput_y = (int16_t) ((gyroData.gyroY - offsetY1) - (phoneGyroData.gyroY))/2.0;
-  gyroInputs[3*i+1] = gyroInput_y;
-
-  int16_t gyroInput_z = (int16_t) ((gyroData.gyroZ - offsetZ1) - (phoneGyroData.gyroZ))/2.0;
-  gyroInputs[3*i+2] = gyroInput_z;
-
-  // Copies the FIFO and sorts it from oldest to newest data
-  sortFIFO();
   // Sends the copied and sorted FIFO into the model
-  // Uncomment when the bluetooth is figured out. Also, we gotta figure out how to send this number to the phone.
-  int32_t model_output = testModel_predict(gyroToModel, 3); // Do something with this output (0/1)!
-  // Serial.printf("%f, %f, %f", ((gyroData.gyroX - offsetX1)- (phoneGyroData.gyroX)), ((gyroData.gyroY - offsetY1)- (phoneGyroData.gyroY)), ((gyroData.gyroZ - offsetZ1)- (phoneGyroData.gyroZ)));
-  Serial.printf("%d, %d, %d, %d", gyroInput_x, gyroInput_y, gyroInput_z, model_output);
-  if (model_output) {
-    Serial1.printf("$1,4\n");
+  int32_t model_output = testModel_predict(gyroToModel, 3);
+  // if (model_output) {
+  //   Serial.println("model output");
+  // }
+
+  // If the phone's gyro reports rate of rotation over threshold, set
+  // flag to indicate that the bike is turning. Otherwise if the flag is
+  // already set, decrement the turning flag.
+  float phone_gyro_output = (abs(phoneGyroData.gyroX) + abs(phoneGyroData.gyroY) + abs(phoneGyroData.gyroZ));
+  if (phone_gyro_output > rotation_threshold) {
+    turning = turning_flag_max;
+  } else {
+    turning = (turning > 0) ? (turning - 1) : 0;
   }
 
+  // if (turning) {
+  //   Serial.println("turning");
+  // }
+
+  // Sends message to phone if model output is 1 and bike is not turning
+  if (model_output && (!turning)) {
+    Serial1.printf("$1,4\n");
+  }
+  
   i = (i + 1) % 12;
+
+  // Serial.printf("%f, %f, %f, %f, %f, %f\n", (gyroData.gyroX - offsetX1/9.0), phoneGyroData.gyroX, (gyroData.gyroY - offsetY1/9.0),  phoneGyroData.gyroY, (gyroData.gyroZ - offsetZ1)/9.0, (phoneGyroData.gyroZ));
+  // Serial.printf("gyro[%f, %f, %f], phone[%f, %f, %f]", (gyroData.gyroX - offsetX1/9.0), (gyroData.gyroY - offsetY1/9.0), (gyroData.gyroZ - offsetZ1)/9.0, phoneGyroData.gyroX, phoneGyroData.gyroY, phoneGyroData.gyroZ);
+  // Serial.printf("%f, %f, %f, %f, %f, %f\n", gyroData.gyroX - offsetX1, phoneGyroData.gyroX, (gyroData.gyroY - offsetY1),  phoneGyroData.gyroY, (gyroData.gyroZ - offsetZ1), (phoneGyroData.gyroZ));
+  // Serial.printf("%f, %f, %f", ((gyroData.gyroX - offsetX1)- (phoneGyroData.gyroX)), ((gyroData.gyroY - offsetY1)- (phoneGyroData.gyroY)), ((gyroData.gyroZ - offsetZ1)- (phoneGyroData.gyroZ)));
+  // Serial.printf("%d, %d, %d, %d", gyroInput_x, gyroInput_y, gyroInput_z, model_output);
 
   // Prints LiDAR measurements
   printLidarData();
   delay(5);
 }
+
+void newModelInput() {
+  // Computes the difference in gyro values
+  // Phone data is 3 readings behind IMU data
+  int16_t gyroInput_x = (int16_t) (gyroBuffer1[0] - (phoneGyroData.gyroX))/2.0;
+  gyroInputs[3*i] = gyroInput_x;
+
+  int16_t gyroInput_y = (int16_t) (gyroBuffer1[1] - (phoneGyroData.gyroY))/2.0;
+  gyroInputs[3*i+1] = gyroInput_y;
+
+  int16_t gyroInput_z = (int16_t) (gyroBuffer1[2] - (phoneGyroData.gyroZ))/2.0;
+  gyroInputs[3*i+2] = gyroInput_z;
+
+  // Shifts buffer values
+  for (int k = 0; k < 3; k++) {
+    gyroBuffer3[k] = gyroBuffer2[k];
+    gyroBuffer2[k] = gyroBuffer1[k];
+  }
+  Serial.printf("gyro[%f, %f, %f], phone[%f, %f, %f]\n", gyroBuffer3[0], gyroBuffer3[1], gyroBuffer3[2], phoneGyroData.gyroX, phoneGyroData.gyroY, phoneGyroData.gyroZ);
+
+  gyroBuffer1[0] = (gyroData.gyroX - offsetX1)/9.0;
+  gyroBuffer1[1] = (gyroData.gyroY - offsetY1)/9.0;
+  gyroBuffer1[2] = (gyroData.gyroZ - offsetZ1)/9.0;
+
+  // Copies the FIFO and sorts it from oldest to newest data
+  sortFIFO();
+}
+
 
 // Sorts FIFO so it is ready to insert into model
 void sortFIFO() {
@@ -121,9 +166,9 @@ void printBluetoothData() {
 
   if (gyro_x + gyro_y + gyro_z == checksum) {
     // Serial.printf(" bluetooth: %f, %f, %f\n", gyro_x, gyro_y, gyro_z);
-    phoneGyroData.gyroX = gyro_x;
-    phoneGyroData.gyroY = gyro_y;
-    phoneGyroData.gyroZ = gyro_z;
+    phoneGyroData.gyroX = gyro_x * 2;
+    phoneGyroData.gyroY = gyro_y * 2;
+    phoneGyroData.gyroZ = gyro_z * 2;
   } else {
     // Serial.print("checksum failed");
   }
@@ -167,6 +212,7 @@ void checkLidarData(TwoWire& Wire, int &previousDistance, int sensor) {
   distance = buf[0] * 0x100 + buf[1] + 10;
   // Serial.printf("%4dmm\t", distance);
 
+  // '10' is the output when a LIDAR sensor does not receive a reading. We manually change this to an arbitrarily high number.
   if (distance == 10) {
     distance = 5000;
   }
@@ -181,13 +227,14 @@ void checkLidarData(TwoWire& Wire, int &previousDistance, int sensor) {
 
   // Serial.printf("sensor: %d, %d, %d\n", sensor, previousDistance, distance);
   // Check for high change from the previous distance
-  if ((previousDistance - distance) >= threshold) {
+  if ((previousDistance != 5000)  && ((previousDistance - distance) >= threshold)) {
     Serial.printf("sensor: %d, %d -> %d\n", sensor, previousDistance, distance); // No object detected
     tone(buzzerPin, 1000); // Buzz with 1000 Hz frequency
     delay(100); // Buzz duration
     noTone(buzzerPin);
     // Serial.printf("sensor %d", sensor);
   }
+
 
   previousDistance = distance;
   // Update previous distance
@@ -260,5 +307,3 @@ void calibrateIMU(MPU6050& imu, GyroData& gyroData, float& offsetX, float& offse
 
   Serial.printf("Offsets calculated - X: %f, Y: %f, Z: %f\n", offsetX, offsetY, offsetZ);
 }
-
-
